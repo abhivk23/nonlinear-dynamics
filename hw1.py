ee28f27cd10d1f6407a9629eb0c6a760c3d125c1
abhivk23 <abhivk23@gmail.com>
2021-04-08 11:15:51 -0500

    homework 1 code and writeup

--- hw1.py
+++ hw1.py
@@ -14,7 +14,7 @@ def recur_rel_error(e_r):
     pass
 
 
-def recur_babylonian(p_0,n):
+def recur_babylonian_sqrt_two(p_0,n):
     """
     recursive implementation of babylonain algorithm
     to find sqrt(2))
@@ -23,17 +23,52 @@ def recur_babylonian(p_0,n):
     if (abs(p_n-p_0) < 0.001):
         return p_0
     else:
-        return recur_babylonian(p_n,n)
+        return recur_babylonian_sqrt_two(p_n,n)
 
-def babylonian(p_0,n):
+def babylonian_sqrt_two(n):
     """
     iterative implementation of babylonian algorithm
     converging on sqrt(2)) and associated relative errors
     """
+    p_0 = float(input("We're inspired to guess: "))
     print("Guess ", p_0, " Relative Error: ", rel_error(p_0,math.sqrt(2)))
     p_n = (p_0+(2/p_0))/2
     while(abs(p_n-p_0) > 0.001 and n>0):
-        print("Guess ", p_n, " Relative Error: ", rel_error(p_n,math.sqrt(2)))
+        print("Guess: ", p_n, " Relative Error: ", rel_error(p_n,math.sqrt(2)))
         p_0 = p_n
         p_n = (p_0+(2/p_0))/2
         n=n-1
+    return p_n
+def babylonian_sqrt(a):
+    """
+    Generalized babylonian sqrt function
+    """
+    if(a==0):
+        return 0
+    elif (a<0):
+        raise Exception("Sqrt argument expected to be positive and real-valued.")
+    else:
+        p_0 = float(input("We're inspired to guess: "))
+        iter_limit = 100
+        while(abs(p_0*p_0 - a) > 0.001 and iter_limit>0):
+            print("Guess ", p_0, " Relative Error: ", rel_error(p_0,math.sqrt(a)))
+            p_0 = (p_0 + (a/p_0))/2
+            iter_limit = iter_limit-1
+        return p_0
+
+def poly_root():
+    """
+    given polynomial f and f_deriv
+    p_n = (p_0 - f(p_0)/f_deriv(p_0))
+    """
+    p_0 = float(input("Initial guess: "))
+    f = p_0*p_0*p_0 - p_0
+    f_deriv = 3*p_0*p_0 - 1
+    p_n = (p_0 - (f/f_deriv))
+    while(abs(p_0 - p_n) > 0.00001):
+        p_0 = p_n
+        f = p_0*p_0*p_0 - p_0
+        f_deriv = 3*p_0*p_0 - 1
+        p_n = (p_0 - (f/f_deriv))
+        print(p_n)
+    return p_n
